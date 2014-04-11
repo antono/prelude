a9aeb903f1b6d0f0b7abc8660eb0c65e3a5ef0ae
Antono Vasiljev <self@antono.info>
2014-04-11 23:33:18 +0300

    Disable global linum mode

--- personal/antono.ui.el
+++ personal/antono.ui.el
@@ -19,17 +19,16 @@
 ;;
 ;; Nice linum padding
 ;;
-(setq linum-format
-      (lambda (line)
-        (propertize
-         (format
-          (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
-            (concat " %" (number-to-string w) "d "))
-          line)
-         'face
-         'linum)))
-
-(global-linum-mode)
+;; (setq linum-format
+;;       (lambda (line)
+;;         (propertize
+;;          (format
+;;           (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+;;             (concat " %" (number-to-string w) "d "))
+;;           line)
+;;          'face
+;;          'linum)))
+;; (global-linum-mode)
 
 ;;
 ;; Speedbar
