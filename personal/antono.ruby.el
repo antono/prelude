3ac7a1c75c2144f2e5df37de0b6860dae0a96fb2
Antono Vasiljev <self@antono.info>
2014-04-15 02:16:05 +0300

    Detect more ruby files

--- personal/antono.ruby.el
+++ personal/antono.ruby.el
@@ -1,2 +1,6 @@
 (prelude-require-package 'projectile-rails)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+(mapcar (lambda (pattern)
+          (add-to-list 'auto-mode-alist `(,pattern . ruby-mode)))
+        '("Gemfile$" "Cheffile$" "Berksfile$" "config.ru$" "Rakefile$" "Guardfile$" "Capfile$" "*\.rb" "\\.rake$" "\\.gemspec$"))
