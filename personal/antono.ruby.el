ae3dd9156c8be710e4e6f1b30f413632057276ce
Antono Vasiljev <self@antono.info>
2014-04-25 12:44:58 +0300

    Feature mode config

--- personal/antono.ruby.el
+++ personal/antono.ruby.el
@@ -9,3 +9,14 @@
    (add-to-list 'auto-mode-alist `(,pattern . ruby-mode)))
  '("Gemfile$" "Cheffile$" "Berksfile$" "config.ru$" "Rakefile$"
    "Guardfile$" "Capfile$" "*\.rb" "\\.rake$" "\\.gemspec$" "\\.rabl$"))
+
+
+(eval-after-load 'feature-mode
+  '(progn
+     (evil-define-key 'normal feature-mode-map
+       (kbd "C-c C-a") 'feature-verify-all-scenarios-in-buffer)
+     (evil-define-key 'normal feature-mode-map
+       (kbd "C-c C-c") 'feature-verify-scenario-at-pos)
+     (evil-define-key 'normal feature-mode-map
+       (kbd "C-]") 'feature-goto-step-definition)))
+(setq feature-cucumber-command "spring cucumber {options} {feature}")
