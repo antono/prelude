13e47a12ce07a331f23edf3593eaeef836126012
Antono Vasiljev <antono.vasiljev@gmail.com>
2014-10-07 23:50:55 +0300

    rbenv

--- personal/antono.ruby.el
+++ personal/antono.ruby.el
@@ -20,3 +20,14 @@
      (evil-define-key 'normal feature-mode-map
        (kbd "C-]") 'feature-goto-step-definition)))
 (setq feature-cucumber-command "spring cucumber {options} {feature}")
+
+(defadvice rspec-compile (around rspec-compile-around)
+  "Use BASH shell for running the specs because of ZSH issues."
+  (let ((shell-file-name "/bin/bash"))
+    ad-do-it))
+
+(ad-activate 'rspec-compile)
+
+; Setting rbenv path
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
